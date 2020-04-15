e0a9cd40802d45e37bb63abe6f1f426cc0c1a4f5
cesarspg3 <cesar_o_@hotmail.com>
2020-04-15 17:15:41 +0200

    log config

--- server/config/config.js
+++ server/config/config.js
@@ -9,12 +9,23 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ===================
+// Vencimiento del token
+// ===================
+process.env.EXPIRATION =  60 * 60 * 24 * 30;
+
+// ===================
+// SEED de autenticación
+// ===================
+process.env.SEED = process.env.SEED || 60 * 60 * 24 * 30;
+
+// ===================
 // BD
 // ===================
 let urlDB;
+console.log(process.env)
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cesarSPG3:KrChafxvRp7XvmoC@cluster0-udnal.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
